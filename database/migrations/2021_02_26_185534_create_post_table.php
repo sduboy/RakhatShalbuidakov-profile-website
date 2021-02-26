47807c8c54ee257008a04fc7048d9955f4ebb682
sduboy <190103297@stu.sdu.edu.kz>
2021-02-27 00:13:14 +0500

    created post table

--- database/migrations/2021_02_26_185534_create_post_table.php
+++ database/migrations/2021_02_26_185534_create_post_table.php
@@ -15,6 +15,8 @@ class CreatePostTable extends Migration
     {
         Schema::create('post', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
         });
     }
